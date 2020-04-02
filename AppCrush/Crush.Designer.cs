2a7a2e91c988e114dec3082ddc23520f138a214d
Thái Vĩnh Toàn <vinhtoan466@gmail.com>
2020-04-02 10:12:13 +0700

    complete

---
+++ AppCrush/Crush.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace AppCrush
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.lbCaption = new System.Windows.Forms.Label();
+            this.bt_dy = new System.Windows.Forms.Button();
+            this.bt_kdy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbCaption
+            // 
+            this.lbCaption.AutoSize = true;
+            this.lbCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCaption.ForeColor = System.Drawing.Color.Red;
+            this.lbCaption.Location = new System.Drawing.Point(12, 9);
+            this.lbCaption.Name = "lbCaption";
+            this.lbCaption.Size = new System.Drawing.Size(451, 31);
+            this.lbCaption.TabIndex = 0;
+            this.lbCaption.Text = "Em đồng ý làm người yêu anh nhé !!!";
+            // 
+            // bt_dy
+            // 
+            this.bt_dy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_dy.Location = new System.Drawing.Point(18, 82);
+            this.bt_dy.Name = "bt_dy";
+            this.bt_dy.Size = new System.Drawing.Size(167, 46);
+            this.bt_dy.TabIndex = 0;
+            this.bt_dy.Text = "Đồng ý";
+            this.bt_dy.UseVisualStyleBackColor = true;
+            this.bt_dy.Click += new System.EventHandler(this.bt_dy_Click);
+            // 
+            // bt_kdy
+            // 
+            this.bt_kdy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_kdy.Location = new System.Drawing.Point(272, 82);
+            this.bt_kdy.Name = "bt_kdy";
+            this.bt_kdy.Size = new System.Drawing.Size(205, 46);
+            this.bt_kdy.TabIndex = 1;
+            this.bt_kdy.Text = "Không đồng ý";
+            this.bt_kdy.UseVisualStyleBackColor = true;
+            this.bt_kdy.Click += new System.EventHandler(this.bt_kdy_Click);
+            this.bt_kdy.MouseLeave += new System.EventHandler(this.bt_kdy_MouseLeave);
+            this.bt_kdy.MouseHover += new System.EventHandler(this.bt_kdy_MouseHover);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(489, 157);
+            this.Controls.Add(this.bt_kdy);
+            this.Controls.Add(this.bt_dy);
+            this.Controls.Add(this.lbCaption);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crush";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCaption;
+        private System.Windows.Forms.Button bt_dy;
+        private System.Windows.Forms.Button bt_kdy;
+    }
+}
+
---
+++ AppCrush/Crush.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCrush
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        string fileContent = "content.conf";
+        string[] lines = { "Anh đồng ý làm người yêu em nhé <3","Em biết anh sẽ đồng ý mà <3"};
+        public Form()
+        {
+            InitializeComponent();
+            if (!File.Exists(fileContent))
+            {
+                using (File.Create(fileContent)) { };
+            }
+            else
+            {
+                string[] readFile = File.ReadAllLines(fileContent);
+                MessageBox.Show(readFile.Count().ToString());
+                if (readFile.Count() == 2)
+
+                    readFile.CopyTo(lines, 0);
+            }
+            lbCaption.Text = lines[0];
+
+           
+        }
+
+        private void bt_kdy_MouseHover(object sender, EventArgs e)
+        {
+            bt_kdy.Text = "Đồng ý";
+            bt_kdy.ForeColor = Color.Red;
+        }
+
+        private void bt_kdy_MouseLeave(object sender, EventArgs e)
+        {
+            bt_kdy.ForeColor = Color.Black;
+            bt_kdy.Text = "Không đồng ý";
+        }
+
+        private void bt_dy_Click(object sender, EventArgs e)
+        {          
+            MessageBox.Show(lines[1]);
+        }
+
+        private void bt_kdy_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lines[1]);
+        }
+    }
+}
